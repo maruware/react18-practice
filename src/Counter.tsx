85c5172ba462ff3aee5aa5a3c21cd700efa56a51
maruware <me@maruware.com>
2022-02-11 18:00:49 +0900

    Test transition

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,16 +1,22 @@
-import React, { useCallback, useState } from "react";
+import React, { startTransition, useCallback, useState } from "react";
+
+function sleep(ms: number) {
+  const startTime = performance.now();
+  while (performance.now() - startTime < ms);
+}
 
 export const Counter: React.VFC = () => {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
-  const [count3, setCount3] = useState(0);
 
   const handleClick = useCallback(() => {
-    setTimeout(() => {
-      setCount1((count) => count + 1);
-      setCount2((count) => count + 1);
-      setCount3((count) => count + 1);
-    }, 500);
+    setCount1((count) => count + 1);
+    startTransition(() =>
+      setCount2((count) => {
+        sleep(2000);
+        return count + 1;
+      })
+    );
   }, []);
 
   console.log("rendering");
@@ -19,7 +25,6 @@ export const Counter: React.VFC = () => {
     <div>
       <p>{count1}</p>
       <p>{count2}</p>
-      <p>{count3}</p>
       <button onClick={handleClick}>Increment</button>
     </div>
   );
